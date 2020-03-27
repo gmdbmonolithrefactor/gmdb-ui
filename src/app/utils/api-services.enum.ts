e810ee31a1a4ad2f1c9d48c127dc9513511126e7
rlwing <rob@wingjr.com>
2020-03-27 13:27:19 -0700

    fixed proxies to new environment

--- src/app/utils/api-services.enum.ts
+++ src/app/utils/api-services.enum.ts
@@ -1,4 +1,4 @@
 export enum ApiServices {
-  MoviesApi = 'movie-svc/GMDB-MOVIES/gmdb/api/movies/',
-  UsersApi = 'users-svc/GMDB-USERS/gmdb/api/users/'
+  MoviesApi = 'api/movies',
+  UsersApi = 'api/users'
 }
